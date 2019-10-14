755bc7a810da9fed79a53f8fb2daa80c4561b2ce
jissatsu <ichi256@abv.bg>
2019-10-14 14:49:43 +0300

    -zzzZZZ-

--- src/file.c
+++ src/file.c
@@ -1,1 +1,18 @@
 #include "file.h"
+
+int fclines( FILE *fp )
+{
+    register int c, size;
+    c    = 0;
+    size = 0;
+    while ( (c = getc( fp )) != EOF ) {
+        if ( c == '\n' )
+            ++size;
+    }
+    return size;
+}
+
+void fgetlines( FILE *fp, char **lines )
+{
+    
+}
--- src/file.h
+++ src/file.h
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <unistd.h>
 
-size_t _f_clines( FILE *fp );
-size_t _f_glines( FILE *fp, char **lines );
+int  fclines( FILE *fp );
+void fgetlines( FILE *fp, char **lines );
 
 #endif
